073491d5af5262a8970698c9f005c932a28bcc98
Tash Tenner <tashfonetikas@gmail.com>
2019-11-23 13:43:54 +0100

    see list of tasks, with title and body

--- src/views/tasks/TaskAll.js
+++ src/views/tasks/TaskAll.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import taskService from '../../services/taskService';
 
 class TaskAll extends Component {
+  state = {
+    tasks: [],
+    loading: true,
+  }
+
+  async componentDidMount() {
+    try {
+      const tasks = await taskService.getAllTasks()
+      this.setState({
+        tasks,
+        loading: false
+      })
+    } catch (error) { }
+  }
+
   render() {
+    const { tasks, loading } = this.state;
+
     return (
-      <div>
-        TaskAll
-      </div>
+      <>
+        <h1>Roll up your sleeves and work hard! These are your tasks:</h1>
+        {!loading && tasks.map((task) => {
+          return (
+            <div key={task._id}>
+              <p>{task.title}</p>
+              <p>{task.body}</p>
+            </div>
+          )
+        })}
+        {loading && <div>loading...</div>}
+      </>
     );
   }
 }
