4c3be46646e7c975ca07d75012a1823ca7afab46
Daniel Duque <duque@outlook.com>
2019-11-27 11:42:10 +0100

    Created navbar

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -10,26 +10,15 @@ import {
 
 const Topbar = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <LinkContainer to="/"><Navbar.Brand>React-Bootstrap</Navbar.Brand></LinkContainer>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
+      <LinkContainer to="/"><Navbar.Brand>Ironhack To-Do</Navbar.Brand></LinkContainer>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          <NavDropdown title="Actions" id="collasible-nav-dropdown">
+            <LinkContainer to="/tasks/create"><NavDropdown.Item>Create Task</NavDropdown.Item></LinkContainer>
+            <LinkContainer to="/tasks"><NavDropdown.Item>Tasks</NavDropdown.Item></LinkContainer>
           </NavDropdown>
-        </Nav>
-        <Nav>
-          <Nav.Link href="#deets">More deets</Nav.Link>
-          <Nav.Link eventKey={2} href="#memes">
-            Dank memes
-      </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
