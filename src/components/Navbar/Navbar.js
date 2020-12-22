1ba1364a149a6ee2127bf28ead38364a0f8af13d
Carla Navia <cnavia27@gmail.com>
2020-12-22 19:41:29 +0100

    navbar css done

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav
+      className="navbar nav-color is-fixed-top"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="navbar-brand">
+        <a className="navbar-item" href="/">
+          <h1 className="h1-nav">doEAT!</h1>
+        </a>
+      </div>
+      <div>
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <Link to="/new" className="button is-light ">
+                <p className="h1-nav add">AddEAT!</p>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>      
+    </nav>
+  );
+}
+
+export default Navbar;
