0632614c111f6a081c3b8420fc7e3d8cbb717d14
EcaCosca <enriquecoscarelli@gmail.com>
2021-01-14 13:46:59 +0100

    CRUD is done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      newItem: "",
+      list: []
+    }
+  }
+
+  updateInput(key, value){
+    this.setState({
+      [key]: value
+    });
+  }
+  addItem(){
+    const newItem={
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+
+    const list = [...this.state.list];
+
+    list.push(newItem);
+
+    this.setState({
+      list,
+      newItem:""
+    });
+  }
+  deleteItem(id){
+    const list = [...this.state.list];
+
+    const updatedList = list.filter(item => item.id !== id);
+
+    this.setState({list: updatedList})
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        To do:
+        <br/>
+        <input
+          tyoe="text"
+          placeholder="What's next?"
+          value={this.state.newItem}
+          onChange={e => this.updateInput("newItem", e.target.value)}
+        />
+        <button
+          onClick={() => this.addItem()}
+        >
+        Add
+        </button>
+        <br/>
+        <ul>
+          {this.state.list.map(item => {
+            return(
+              <li key={item.id}>
+                {item.value}
+                <button
+                  onClick={() => this.deleteItem(item.id)}
+                >
+                Delete
+                </button>
+              </li>
+            )
+          })}
+        </ul>
       </div>
     );
   }
