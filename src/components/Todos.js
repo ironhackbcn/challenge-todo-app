d536fde648df8e2cb01c51d559b2aacbd72d1dd3
Juan Miguel Sansinenea <juanmi.sansinenea@gmail.com>
2021-01-14 11:51:59 +0100

    successfully routing to components / Todos

---
+++ src/components/Todos.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Todos extends Component {
+    render() {
+        return (
+            <div>
+                ello!
+            </div>
+        )
+    }
+}
