b73643fa8b9c9ad72027104a7dbade5b56eb8a64
Juan Miguel Sansinenea <juanmi.sansinenea@gmail.com>
2021-01-14 12:25:14 +0100

    rendeing todo list

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default class Todos extends Component {
+    state = {
+        todosArr: [],
+    }
     render() {
         return (
             <div>
-                ello!
+            
+                {this.state.todosArr.map ((oneTodo)=>(
+                    <Link key={oneTodo._id} to={`/todos/${oneTodo._id}`}>
+                        <div className="one-todo">
+                        {oneTodo.title}
+                        </div>
+                    </Link> 
+                ))} 
             </div>
         )
     }
+    componentDidMount() {
+    
+        axios
+          .get(`http://localhost:4000/api/v1/todos`)
+          .then((apiResponse) => {
+         
+            this.setState({ todosArr: apiResponse.data });
+            console.log(this.state.todosArr[0].title)
+
+          })
+          .catch((err) => console.log(err));
+      }
 }
